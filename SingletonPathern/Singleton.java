2d6bd10852ca762f8bd48e255656266989fe6d3c
unknown <kaitong.wu@cjmkt.com>
2017-02-17 15:14:18 +0800

    Factory Pattern

---
+++ SingletonPathern/Singleton.java
@@ -0,0 +1,15 @@
+package SingletonPathern;
+
+public class Singleton {
+	
+	private static Singleton uniqueInstance;
+	
+	private Singleton() {}
+	
+	public static Singleton getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance= new Singleton();
+		}
+		return uniqueInstance;
+	}
+}
